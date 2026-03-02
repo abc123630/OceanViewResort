db95be0c254969429dc927b06ff9bb0806c672cf
PLUS VISION PC <PLUS VISION PC@DESKTOP-HRJCPAR>
2026-03-02 14:29:37 -0800

    Test

--- OceanViewResort/src/test/java/com/ocean/service/ReservationServiceTest.java
+++ OceanViewResort/src/test/java/com/ocean/service/ReservationServiceTest.java
@@ -1,5 +1,29 @@
+
 package com.ocean.service;
 
-public class ReservationServiceTest {
+import com.ocean.model.Reservation;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-}
+class ReservationServiceTest {
+
+	@Test
+	void testCalculateBill() {
+
+	    Reservation r = new Reservation(
+	            1,
+	            "John",
+	            "Colombo",
+	            "0771234567",
+	            101,
+	            "2026-03-01",
+	            "2026-03-05"
+	    );
+
+	    r.setRatePerNight(100.0);
+
+	    double bill = ReservationService.calculateBill(r);
+
+	    assertEquals(400.0, bill, 0.001);
+	}
+}	
